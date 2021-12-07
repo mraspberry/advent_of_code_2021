31ae13991ef1d4f34b7592a5fdb2a86b6cca6d5b
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2021-12-06 19:10:48 -0600

    completed day 3 problem 2 and ran cargo fmt

--- d3p1/src/main.rs
+++ d3p1/src/main.rs
@@ -4,12 +4,15 @@ use std::fs;
 #[derive(Debug, Clone, Copy)]
 struct Column {
     zeros: u32,
-    ones: u32
+    ones: u32,
 }
 
 impl Column {
     fn new() -> Column {
-        Column { zeros: 0u32, ones: 0u32 }
+        Column {
+            zeros: 0u32,
+            ones: 0u32,
+        }
     }
 
     fn most_common(&self) -> &str {
@@ -32,36 +35,37 @@ impl Column {
 fn process_readings(readings: String) -> HashMap<usize, Column> {
     let mut columns: HashMap<usize, Column> = HashMap::new();
     for line in readings.lines() {
-//        dbg!(line);
+        //        dbg!(line);
         if line.trim().is_empty() {
-            continue
+            continue;
         }
         for (i, item) in line.chars().enumerate() {
             let mut column = columns.entry(i).or_insert(Column::new());
             match item {
                 '1' => column.ones += 1,
                 '0' => column.zeros += 1,
-                _ => continue
+                _ => continue,
             };
         }
     }
-    return columns
+    return columns;
 }
 fn solve(readings: String) -> u32 {
     let columns = process_readings(readings);
-//    dbg!(&columns);
+    //    dbg!(&columns);
     let mut gamma = String::new();
     let mut epsilon = String::new();
-    for i in {0..columns.len()}.into_iter() {
-//        dbg!(i);
+    for i in { 0..columns.len() }.into_iter() {
+        //        dbg!(i);
         gamma.push_str(columns[&i].most_common());
-//        dbg!(&gamma);
+        //        dbg!(&gamma);
         epsilon.push_str(columns[&i].least_common());
-//        dbg!(&epsilon);
+        //        dbg!(&epsilon);
     }
-//    dbg!(&gamma);
-//    dbg!(&epsilon);
-    u32::from_str_radix(gamma.as_str(), 2).unwrap() * u32::from_str_radix(epsilon.as_str(), 2).unwrap()
+    //    dbg!(&gamma);
+    //    dbg!(&epsilon);
+    u32::from_str_radix(gamma.as_str(), 2).unwrap()
+        * u32::from_str_radix(epsilon.as_str(), 2).unwrap()
 }
 fn main() {
     let readings = fs::read_to_string("input.txt").expect("Unable to read input.txt");
@@ -72,18 +76,8 @@ fn main() {
 fn test_example() {
     let ans: u32 = 198;
     let input: Vec<&str> = vec![
-        "00100",
-        "11110",
-        "10110",
-        "10111",
-        "10101",
-        "01111",
-        "00111",
-        "11100",
-        "10000",
-        "11001",
-        "00010",
-        "01010",
+        "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001",
+        "00010", "01010",
     ];
     assert_eq!(ans, solve(input.join("\n")));
 }
---
+++ d3p2/src/main.rs
@@ -0,0 +1,114 @@
+use std::collections::HashMap;
+use std::fs;
+
+#[derive(Debug, Clone, Copy)]
+struct Column {
+    zeros: u32,
+    ones: u32,
+}
+
+impl Column {
+    fn new() -> Column {
+        Column {
+            zeros: 0u32,
+            ones: 0u32,
+        }
+    }
+
+    fn most_common(&self) -> char {
+        if self.zeros > self.ones {
+            '0'
+        } else {
+            '1'
+        }
+    }
+
+    fn least_common(&self) -> char {
+        if self.ones < self.zeros {
+            '1'
+        } else {
+            '0'
+        }
+    }
+}
+
+fn populate_columns(readings: &Vec<&str>) -> HashMap<usize, Column> {
+    let mut columns: HashMap<usize, Column> = HashMap::new();
+    for r in readings.into_iter() {
+        if r.trim().is_empty() {
+            continue;
+        }
+        for (i, item) in r.chars().enumerate() {
+            let mut column = columns.entry(i).or_insert(Column::new());
+            match item {
+                '1' => column.ones += 1,
+                '0' => column.zeros += 1,
+                _ => continue,
+            };
+        }
+    }
+    return columns;
+}
+
+fn calc_ogenerator_rating(readings: &String) -> u32 {
+    let mut readings = readings.split_whitespace().collect::<Vec<_>>();
+    //    dbg!(&readings);
+    let mut columns = populate_columns(&readings);
+    let mut index: usize = 0;
+    while readings.len() > 1 {
+        let col = columns.get(&index).unwrap();
+        //        dbg!(col);
+        //        dbg!(index);
+        let most_common = col.most_common();
+        readings = readings
+            .into_iter()
+            .filter(|r| r.chars().nth(index).unwrap() == most_common)
+            .collect();
+        //        dbg!(&readings);
+        index += 1;
+        columns = populate_columns(&readings);
+    }
+    u32::from_str_radix(readings[0], 2).unwrap()
+}
+
+fn calc_scrubber_rating(readings: &String) -> u32 {
+    let mut readings = readings.split_whitespace().collect::<Vec<_>>();
+    //    dbg!(&readings);
+    let mut columns = populate_columns(&readings);
+    let mut index: usize = 0;
+    while readings.len() > 1 {
+        let col = columns.get(&index).unwrap();
+        let least_common = col.least_common();
+        readings = readings
+            .into_iter()
+            .filter(|r| r.chars().nth(index).unwrap() == least_common)
+            .collect();
+        //        dbg!(&readings);
+        index += 1;
+        columns = populate_columns(&readings);
+    }
+    u32::from_str_radix(readings[0], 2).unwrap()
+}
+
+fn solve(readings: String) -> u32 {
+    let ogenerator = calc_ogenerator_rating(&readings);
+    let scrubber = calc_scrubber_rating(&readings);
+    //    dbg!(ogenerator);
+    //    dbg!(scrubber);
+    ogenerator * scrubber
+}
+
+fn main() {
+    let readings = fs::read_to_string("input.txt").expect("Unable to read input.txt");
+    println!("{}", solve(readings));
+}
+
+#[test]
+fn test_example() {
+    let ans: u32 = 230;
+    let input: Vec<&str> = vec![
+        "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001",
+        "00010", "01010",
+    ];
+    assert_eq!(ans, solve(input.join("\n")));
+}
